833cf17c1634beb3b6e125d6a1fde109ec82438b
Dmytro Regeta <dmitry.regeta@dataart.com>
2016-07-29 12:49:28 +0300

    Initial commit;

---
+++ SingletonPattern/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace SingletonPattern
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var product1 = SingletonProduct.Creat();
+            var product2 = SingletonProduct.Creat();
+
+            Console.WriteLine($"Product 1 size: {product1.GetSize()}");
+            Console.WriteLine($"Product 2 size: {product2.GetSize()}");
+
+            product1.ChangeSize(1);
+            product2.ChangeSize(2);
+
+            Console.WriteLine($"Product 1 size: {product1.GetSize()}");
+            Console.WriteLine($"Product 2 size: {product2.GetSize()}");
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ SingletonPattern/SingletonProduct.cs
@@ -0,0 +1,33 @@
+﻿namespace SingletonPattern
+{
+    internal class SingletonProduct
+    {
+        private static SingletonProduct _singletonProduct;
+
+        private SingletonProduct()
+        {
+        }
+
+        private int _size { get; set; }
+
+        public static SingletonProduct Creat()
+        {
+            if (_singletonProduct == null)
+            {
+                _singletonProduct = new SingletonProduct();
+                return _singletonProduct;
+            }
+            return _singletonProduct;
+        }
+
+        public void ChangeSize(int size)
+        {
+            _size = size;
+        }
+
+        public int GetSize()
+        {
+            return _size;
+        }
+    }
+}
